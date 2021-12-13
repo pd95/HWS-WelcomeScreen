8661116e294f708129f0cbe6f9403dd634f40db8
Philipp <pd95@users.noreply.github.com>
2021-12-13 20:00:02 +0100

    Initial Commit

---
+++ WelcomeScreen/WelcomeScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeScreenApp.swift
+//  WelcomeScreen
+//
+//  Created by Philipp on 13.12.21.
+//
+
+import SwiftUI
+
+@main
+struct WelcomeScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
